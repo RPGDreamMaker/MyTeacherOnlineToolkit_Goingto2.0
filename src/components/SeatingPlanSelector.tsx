ad05446b2dd12f51aee544f573bcf9b28102dd27
RPGDreamMaker <sylvainrudault@gmail.com>
2025-08-29 09:38:30 -1000

    Updated SeatingPlanSelector.tsx

--- src/components/SeatingPlanSelector.tsx
+++ src/components/SeatingPlanSelector.tsx
@@ -206,7 +206,8 @@ export default function SeatingPlanSelector() {
                   <option 
                     key={plan.id} 
                     style={{
-                      backgroundColor: plan.color ? `${plan.color}20` : undefined
+                      backgroundColor: plan.color ? `${plan.color}20` : undefined,
+                      fontWeight: 'bold'
                     }}
                     value={plan.id}
                   >
